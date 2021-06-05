683c8af0d43be882d7ae5c732411e2a273684c03
Kayalvizhi <61958429+Kayal-Sampath@users.noreply.github.com>
2021-06-05 13:15:32 +0530

    Update sortdocuments.py

--- sortdocuments.py
+++ sortdocuments.py
@@ -1,7 +1,7 @@
 
-for i in range(200,201):
-    f2 = open('/home/kayal/Desktop/Dataset/Task_2/embed/E1/em'+str(i)+'.txt','w+')       
-    fo = open('/home/kayal/Desktop/Dataset/Task_2/embed/E/em'+str(i)+'.txt','r')
+for i in range(1,201):
+    f2 = open('E1/em'+str(i)+'.txt','w+')       
+    fo = open('em'+str(i)+'.txt','r')
     x = fo.readlines()
     sort_key = lambda line: float(line.split('  ', 1)[1])
     sorted_x = sorted(x, key=sort_key, reverse=True)
@@ -10,12 +10,3 @@ for i in range(200,201):
         if count<5:
             f2.write(s)
             count=count+1  
-""" 
-ff=open('/home/kayal/Desktop/Dataset/Task_2/embed/whole_emb.txt','w+')
-for i in range(1,201):
-    f = open('/home/kayal/Desktop/Dataset/Task_2/embed/E1/em'+str(i)+'.txt','r')
-    k=f.read()
-    ff.write('Current_Case_'+str(i)+'\n')
-    ff.write(str(k))
-    ff.write('\n')    
-"""   
